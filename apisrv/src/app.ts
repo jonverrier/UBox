bd8cb596500a6ef60d378539b14ddee996d1bbc8
JonathanVerrier <jonathanverrier@hotmail.com>
2021-08-06 16:25:03 +0100

    Added home page - no function as yet

--- apisrv/src/app.ts
+++ apisrv/src/app.ts
@@ -1,9 +1,55 @@
 
 import express from 'express'
+import path from 'path';
+import errorHandler from 'errorhandler';
+import compression from 'compression';
 
-const app = express()
+// Control variables for development / production
+var inDevelopment = false;
+if (process.env.NODE_ENV === 'development') {
+   inDevelopment = true;
+   console.log('Using development options');
+} else {
+   console.log('Using production options');
+}
 
+// Initialize status middleware - files, cache etc
+// Set cache parameter to one day unless in development, and include 'dev' directory if we are in development mode
+// Note: do this BEFORE adding sessions, else sessions get called multiple times, 
+// once for each asset including static ones
+var cacheAge = 86400000; // 86400000 is One Day
+if (inDevelopment)
+   cacheAge = 0;
 
+// Insert versioning middleware before static
+var currentVersion = '0.1';
+// var versionator = require('versionator').create(currentVersion);
+
+var app = express();
+
+// Force https
+function requireHTTPS(req, res, next) {
+   // The 'x-forwarded-proto' check is for Heroku
+   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+   }
+   next();
+}
+
+app.use(function (req, res, next) {
+   return requireHTTPS(req, res, next);
+});
+
+// Use compression for static files
+app.use(compression());
+
+// Allow files from ../public to be used 
+app.use(express.static(path.join(__dirname, '../public')));
+
+if (inDevelopment) {
+   // Set error handler. 
+   app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+}
 
 app.get('/ping', (req, res) => {
    res.send('pong')
